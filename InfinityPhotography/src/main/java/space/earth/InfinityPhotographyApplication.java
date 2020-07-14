2a19affe956eca431cb7bc3bc332a1fd768e6cb0
Jessica <Jessica@192.168.0.11>
2020-07-13 23:56:29 -0400

    Adding Springboot

---
+++ InfinityPhotography/src/main/java/space/earth/InfinityPhotographyApplication.java
@@ -0,0 +1,13 @@
+package space.earth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityPhotographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityPhotographyApplication.class, args);
+	}
+
+}
